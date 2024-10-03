b23df06749247f5fea037b7d2249d2d3a6258493
Suleiman Suleiman <ssuleiman955@gmail.com>
2024-10-03 14:48:06 -0500

    adding failing tests

--- Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/CalculatorTests.cs
@@ -23,5 +23,27 @@ namespace Calculator.Tests
             var result = Calculator.Multiply(4, 3);
             Assert.That(result, Is.EqualTo(12));
         }
+
+        // Failing tests
+        [Test]
+        public void Add_WhenCalled_ReturnsIncorrectSum()
+        {
+            var result = Calculator.Add(1, 2);
+            Assert.That(result, Is.EqualTo(4)); // This test will fail
+        }
+
+        [Test]
+        public void Subtract_WhenCalled_ReturnsIncorrectDifference()
+        {
+            var result = Calculator.Subtract(5, 3);
+            Assert.That(result, Is.EqualTo(1)); // This test will fail
+        }
+
+        [Test]
+        public void Multiply_WhenCalled_ReturnsIncorrectProduct()
+        {
+            var result = Calculator.Multiply(4, 3);
+            Assert.That(result, Is.EqualTo(11)); // This test will fail
+        }
     }
 }
